4a87ec0f8408272f8d486c1c788fb5e5494f8d7b
Yoruyoru <33980541+JoshM-Yoru@users.noreply.github.com>
2022-07-06 00:47:25 -0400

    Add files via upload
    App is basically complete. Just needs  2 things: Needs more responsiveness from the CSS by either using clamp or media queries; js file needs refactoring as well as appropriate async/await functionality with try/catch

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 
 const key = "2f8ab8fe8286470480231710220207";
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 //GETS GEOLOCATION FROM BROWSER
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -33,21 +57,42 @@ const whereAmI = async function () {
   }
 };
 
+//GETS AND SETS HOURLY WEATHER FOR CURRENT DAY
 const getHourlyWeather = function (weather) {
   for (let i = 0; i < weather.length; i++) {
     document
       .querySelector(`.img-${i}`)
       .setAttribute("src", weather[i].condition.icon);
 
-    document.querySelector(`.temp-${i}`).innerHTML = Math.floor(
-      weather[i].temp_f
-    );
+    document.querySelector(`.temp-${i}`).innerHTML =
+      Math.floor(weather[i].temp_f) + "&deg";
   }
 };
 
+//GETS AND SETS FORECAST
+const getForecast = async function (weather) {
+  for (let i = 0; i < weather.length; i++) {
+    let parsedTime = new Date(JSON.parse(JSON.stringify(weather[i].date)));
+
+    let day = days[parsedTime.getDay()];
+    document.querySelector(`.day-${i}`).innerHTML = day;
+
+    document.querySelector(`.day-0`).style.fontWeight = "bold";
+    document.querySelector(`.day-0`).innerHTML = "Today";
+
+    document
+      .querySelector(`.conditions-${i}`)
+      .setAttribute("src", weather[i].day.condition.icon);
+    document.querySelector(`.high-${i}`).innerHTML =
+      Math.floor(weather[i].day.maxtemp_f) + "&deg";
+    document.querySelector(`.low-${i}`).innerHTML =
+      Math.floor(weather[i].day.mintemp_f) + "&deg";
+  }
+};
+
+//GETS TIME
 const getLocationTime = async function (lat, lng) {
-  const locationTime =
-    "http://api.timezonedb.com/v2.1/get-time-zone?key=V69MTVCHXA2T&format=json&by=position&lat=40.689247&lng=-74.044502";
+  const locationTime = `http://api.timezonedb.com/v2.1/get-time-zone?key=V69MTVCHXA2T&format=json&by=position&lat=${lat}&lng=${lng}`;
 
   const response = await fetch(locationTime);
   const data = await response.json();
@@ -55,44 +100,90 @@ const getLocationTime = async function (lat, lng) {
   return data.formatted;
 };
 
-//GETS AND SETS WEATHER INFO
-const gettingWeather = async (key, city) => {
-  let location = await whereAmI();
+//RENDERS WEATHER INFO
+const renderWeather = async (key, city) => {
   if (!city) {
-    city = location;
+    city = await whereAmI();
   }
 
-  let locationTime = await getLocationTime();
-
   const forecastWeather = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=10&aqi=no&alerts=no`;
-
   const responseForecastWeather = await fetch(forecastWeather);
   const dataForecastWeather = await responseForecastWeather.json();
 
   console.log(dataForecastWeather);
-
+  //SETS CURRENT INFO
   document.querySelector(".city").innerHTML = dataForecastWeather.location.name;
-
   document.querySelector(".temperature").innerHTML =
     Math.floor(dataForecastWeather.current.temp_f) + "&deg";
-
   document.querySelector(".current-status").innerHTML =
     dataForecastWeather.current.condition.text;
-
   document.querySelector(".current-high-low").innerHTML = `H:${Math.floor(
     dataForecastWeather.forecast.forecastday[0].day.maxtemp_f
   )}&deg L:${Math.floor(
     dataForecastWeather.forecast.forecastday[0].day.mintemp_f
   )}&deg`;
 
-  document.querySelector(".date-time").innerHTML = locationTime;
+  //SETS DATE AND TIME
+  let locationTime = await getLocationTime(
+    dataForecastWeather.location.lat,
+    dataForecastWeather.location.lon
+  );
 
+  const parsedTime = new Date(JSON.parse(JSON.stringify(locationTime)));
+
+  let month = months[parsedTime.getMonth()];
+  let day = days[parsedTime.getDay()];
+  let date = parsedTime.getDate();
+  let year = parsedTime.getFullYear();
+  let min = parsedTime.getMinutes();
+  let hour = parsedTime.getHours();
+  let time;
+
+  if (hour > 12) {
+    time = hour - 12 + ":" + min + " " + "PM";
+  } else {
+    time = hour + ":" + min + " " + "AM";
+  }
+
+  document.querySelector(".date-time").innerHTML =
+    time + " " + "-" + " " + day + " " + month + " " + date + " " + year;
+
+  //SETS HOURLY WEATHER
   getHourlyWeather(dataForecastWeather.forecast.forecastday[0].hour);
 
+  document
+    .querySelector(".temp-" + hour)
+    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+  let timeNow = document.querySelector(".hour-" + hour);
+
+  timeNow.style.fontWeight = "bold";
+  timeNow.style.color = "white";
+  timeNow.innerHTML = "Now";
+
+  //SETS MULTIDAY FORECAST
+  getForecast(dataForecastWeather.forecast.forecastday);
+
   document.querySelector(".container").style.opacity = 1;
+
+  let background = dataForecastWeather.current.condition.text.toLowerCase();
+
+  const backgroundArray = ["cloudy", "night", "rain", "storm", "snow"];
+
+  for (let i = 0; i < backgroundArray.length; i++) {
+    if (background.includes(backgroundArray[i])) {
+      background = backgroundArray[i];
+    }
+    document
+      .querySelector(`body`)
+      .setAttribute(
+        "style",
+        "background-image: url(./backgrounds/" + background + ".jpg)"
+      );
+  }
 };
 
-gettingWeather(key);
+renderWeather(key);
 
 //GETS INPUT FROM USER TO SEARCH FOR A SPECIFIC LOCATION
 document.querySelector(".submit").addEventListener("click", function (e) {
@@ -101,5 +192,5 @@ document.querySelector(".submit").addEventListener("click", function (e) {
 
   document.querySelector("input").value = "Please enter a city.";
 
-  gettingWeather(key, city);
+  renderWeather(key, city);
 });
